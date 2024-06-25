76681ac2a814f1ba73efc96abf90318be3e86d17
xiaohuoni <xiaohuoni@dingtalk.com>
2024-06-25 10:49:52 +0800

    feat: antd-mobile

--- demo/src/pages/home/index.tsx
+++ demo/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useRequest } from 'ahooks';
 import { useState } from 'react';
 import { fakeAnalysisChartData } from 'src/services/api';
-
+import { Button } from 'antd-mobile';
 function HomePage() {
   const { loading, data } = useRequest(fakeAnalysisChartData);
   const [count, setCount] = useState(0);
@@ -14,13 +14,13 @@ function HomePage() {
       Hello Mako!
       <p>count: {count}</p>
       <p>
-        <button
+        <Button
           onClick={() => {
             setCount(count + 1);
           }}
         >
           count + 1
-        </button>
+        </Button>
       </p>
       <p>request data: {JSON.stringify(data)}</p>
     </div>
